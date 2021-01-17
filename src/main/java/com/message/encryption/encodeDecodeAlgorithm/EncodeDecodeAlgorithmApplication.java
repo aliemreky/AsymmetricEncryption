0dd5c9976da0a9406230a86b4e379c080e559088
aliemreky <aliemrekaya@hotmail.com>
2021-01-17 17:27:28 +0300

    init mysql and spring boot

---
+++ src/main/java/com/message/encryption/encodeDecodeAlgorithm/EncodeDecodeAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.message.encryption.encodeDecodeAlgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncodeDecodeAlgorithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EncodeDecodeAlgorithmApplication.class, args);
+    }
+
+}
